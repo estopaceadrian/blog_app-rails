90d3042833f09b5859339eec913e799c48764b24
Adrian Estopace <estopaceadrian@gmail.com>
2022-10-04 21:13:18 +0800

    created notification

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -4,8 +4,34 @@ export default class extends Controller {
   initialize() {}
   connect() {}
   toggleForm(event) {
-    console.log('I clockedo n the edit button');
     event.preventDefault();
     event.stopPropagation();
+    const formId = event.params['form'];
+    const commentBodyId = event.params['body'];
+    const editButtonID = event.params['edit'];
+
+    const form = document.getElementById(formId);
+    const commentBody = document.getElementById(commentBodyId);
+    const editButton = document.getElementById(editButtonID);
+
+    form.classList.toggle('d-none');
+    form.classList.toggle('mt-4');
+    commentBody.classList.toggle('d-none');
+    this.toggleEditButton(editButton);
+  }
+
+  toggleEditButton(editButton) {
+    if (editButton.innerText === 'Edit') {
+      editButton.innerText = 'Cancel';
+      this.toggleEditButtonClass(editButton);
+    } else {
+      editButton.innerText = 'Edit';
+      this.toggleEditButtonClass(editButton);
+    }
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle('btn-secondary');
+    editButton.classList.toggle('btn-warning');
   }
 }
